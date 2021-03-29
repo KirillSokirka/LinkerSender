5253ad7f0881b18c697a1875cdb948e943bf2c5a
KirillSokirka <sokirka2003@gmail.com>
2021-03-29 17:49:24 +0300

    Some PEP fixies

--- bot.py
+++ bot.py
@@ -2,18 +2,19 @@ import config
 
 import telebot
 from telebot import types
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.Token)
-
-from flask import Flask, request
 server = Flask(__name__)
+
 
 @server.route('/' + config.Token, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return "!",200
+    return "!", 200
+
 
 @server.route("/")
 def webhook():
@@ -21,21 +22,22 @@ def webhook():
     bot.set_webhook(url='https://linkersender.herokuapp.com/' + config.Token)
     return "!", 200
 
-@bot.message_handler(commands = ['start'])
+
+@bot.message_handler(commands=['start'])
 def say_hello(message):
     bot.send_message(message.chat.id,
-                     'Привіт, я <b>LinkerSender</b>!'+
-                     'Я створений, щоб вибирати всі посилання з твої пошти ' +
+                     'Привіт, я <b>LinkerSender</b>!' +
+                     '\nЯ створений, щоб вибирати всі посилання з твої пошти ' +
                      'та надсилати їх тобі у Telegram', parse_mode='html')
     animation = open(config.HelloSticker, 'rb')
     bot.send_animation(message.chat.id, animation)
+
 
 @bot.message_handler(commands=['enter_channel'])
 def send_link(message):
     bot.send_message(message.chat.id, 'Посилання на канал - ' + config.LinkToChannel)
 
+
 @bot.message_handler(content_types=['text'])
 def post_to_channel(message):
-    bot.send_message(config.ChannelName, message.text)
-
-
+    bot.send_message(message.chat.id, 'ляп')
