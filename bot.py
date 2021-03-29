35877b731de6a0af1ff08f34a92b8c01b2c006b2
KirillSokirka <sokirka2003@gmail.com>
2021-03-29 17:37:23 +0300

    Adding funciton to post in channel

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(config.Token)
 from flask import Flask, request
 server = Flask(__name__)
 
-@server.route('/'+config.Token, methods=['POST'])
+@server.route('/' + config.Token, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = types.Update.de_json(json_string)
@@ -29,4 +29,13 @@ def say_hello(message):
                      'та надсилати їх тобі у Telegram', parse_mode='html')
     animation = open(config.HelloSticker, 'rb')
     bot.send_animation(message.chat.id, animation)
+
+@bot.message_handler(commands=['enter_channel'])
+def send_link(message):
+    bot.send_message(message.chat.id, 'Посилання на канал - ' + config.LinkToChannel)
+
+@bot.message_handler(content_types=['text'])
+def post_to_channel(message):
+    bot.send_message(config.ChannelName, message)
+
 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import config
 from bot import server
 
 import os
