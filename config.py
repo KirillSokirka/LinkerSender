76ddb8a60f10493904bbb72cdf3255a2ff199ef9
KirillSokirka <sokirka2003@gmail.com>
2021-03-29 18:00:45 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 Token = '1482873603:AAEQpbX5FTxkZBu_QZwfVU0k--5MM8BJhwg'
 LinkToChannel = 'https://t.me/joinchat/9wB4axapR380Yjgy'
-ChannelName = '@Links'
+ChannelName = "@links"
 
 HelloSticker = 'AnimatedSticker.tgs'
